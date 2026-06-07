c083aa3f278d334edc8df65aa6b30fc598a8d1e0
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-06-08 00:05:41 +0200

    Implement ReconnectionPolicy class and corresponding unit tests

---
+++ lib/reconnection/ReconnectionPolicy.h
@@ -0,0 +1,29 @@
+class ReconnectionPolicy
+{
+private:
+    bool isArmed = false;
+    bool isEliminated = false;
+    int connectionFailuresCount = 0;
+
+public:
+    void OnAuthSuccess()
+    {
+        isArmed = true;
+        connectionFailuresCount = 0;
+    }
+
+    void OnReconnectFailed()
+    {
+        if (isArmed)
+        {
+            connectionFailuresCount++;
+            if (connectionFailuresCount >= 3)
+                isEliminated = true;
+        }
+    }
+
+    bool IsEliminated() const
+    {
+        return isEliminated;
+    }
+};
--- test/test_reconnection/test_reconnection.cpp
+++ test/test_reconnection/test_reconnection.cpp
@@ -1,6 +1,51 @@
 #include <gtest/gtest.h>
+#include "ReconnectionPolicy.h"
 
-int main(int argc, char **argv) {
+TEST(ReconnectionPolicyTest, UnarmedPolicySurvivesAFailure)
+{
+    ReconnectionPolicy policy;
+    policy.OnReconnectFailed();
+    EXPECT_FALSE(policy.IsEliminated());
+}
+
+TEST(ReconnectionPolicyTest, ArmedPolicyIsEliminatedAfterThreeFailures)
+{
+    ReconnectionPolicy policy;
+    policy.OnAuthSuccess();
+    for (int i = 0; i < 3; i++)
+    {
+        policy.OnReconnectFailed();
+    }
+    EXPECT_TRUE(policy.IsEliminated());
+}
+
+TEST(ReconnectionPolicyTest, AuthSuccessResetsTheFailureCount)
+{
+    ReconnectionPolicy policy;
+    policy.OnAuthSuccess();
+    for (int i = 0; i < 2; i++)
+    {
+        policy.OnReconnectFailed();
+    }
+    policy.OnAuthSuccess();
+    policy.OnReconnectFailed();
+    EXPECT_FALSE(policy.IsEliminated());
+}
+
+TEST(ReconnectionPolicyTest, EliminatedPolicyStaysEliminatedAfterAuthSuccess)
+{
+    ReconnectionPolicy policy;
+    policy.OnAuthSuccess();
+    for (int i = 0; i < 3; i++)
+    {
+        policy.OnReconnectFailed();
+    }
+    policy.OnAuthSuccess();
+    EXPECT_TRUE(policy.IsEliminated());
+}
+
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
