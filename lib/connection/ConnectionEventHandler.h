c0a7119157535967b03f9c957f30e776ae9954ed
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-06-17 13:19:34 +0200

    chore(connection): route received messages

--- lib/connection/ConnectionEventHandler.h
+++ lib/connection/ConnectionEventHandler.h
@@ -1,20 +1,32 @@
 #ifndef CONNECTION_EVENT_HANDLER_H
 #define CONNECTION_EVENT_HANDLER_H
 
+#include <string>
 #include "ReconnectionPolicy.h"
+#include "HubMessageDispatcher.h"
+#include "MessageDeserializer.h"
 
 class ConnectionEventHandler
 {
 private:
     ReconnectionPolicy &reconnectionPolicy;
+    HubMessageDispatcher &hubMessageDispatcher;
+    MessageDeserializer messageDeserializer;
+
 public:
-    ConnectionEventHandler(ReconnectionPolicy &reconnectionPolicy) : reconnectionPolicy(reconnectionPolicy)
+    ConnectionEventHandler(ReconnectionPolicy &reconnectionPolicy, HubMessageDispatcher &hubMessageDispatcher)
+        : reconnectionPolicy(reconnectionPolicy), hubMessageDispatcher(hubMessageDispatcher)
     {
     }
 
     void OnConnectionLost()
     {
         reconnectionPolicy.OnReconnectFailed();
+    }
+
+    void OnMessageReceived(const std::string &message)
+    {
+        hubMessageDispatcher.Dispatch(messageDeserializer.ExtractType(message));
     }
 };
 
--- test/test_connection/test_connection.cpp
+++ test/test_connection/test_connection.cpp
@@ -1,11 +1,19 @@
 #include <gtest/gtest.h>
+#include "../mocks/GameMocks.h"
 #include "ReconnectionPolicy.h"
 #include "ConnectionEventHandler.h"
+#include "BuzzerBehavior.h"
+#include "HubMessageDispatcher.h"
 
 TEST(ConnectionEventHandlerTest, BuzzerEliminatedAfterAThreeTimesDisconnection)
 {
+    MockHubMessageSender mockHubMessageSender;
+    MockLocalFeedback mockLocalFeedback;
+    BuzzerBehavior buzzerBehavior{mockHubMessageSender, mockLocalFeedback};
+    HubMessageDispatcher dispatcher{buzzerBehavior};
     ReconnectionPolicy policy;
-    ConnectionEventHandler handler(policy);
+    ConnectionEventHandler handler(policy, dispatcher);
+    
     policy.OnAuthSuccess();
 
     handler.OnConnectionLost();
@@ -13,6 +21,21 @@ TEST(ConnectionEventHandlerTest, BuzzerEliminatedAfterAThreeTimesDisconnection)
     handler.OnConnectionLost();
 
     EXPECT_TRUE(policy.IsEliminated());
+}
+
+TEST(ConnectionEventHandlerTest, SpeedBuzzArmedOnQuestionOpenMessageReceived)
+{
+    MockHubMessageSender mockHubMessageSender;
+    MockLocalFeedback mockLocalFeedback;
+    BuzzerBehavior buzzerBehavior{mockHubMessageSender, mockLocalFeedback};
+    HubMessageDispatcher dispatcher{buzzerBehavior};
+    ReconnectionPolicy policy;
+    ConnectionEventHandler handler(policy, dispatcher);
+
+    EXPECT_CALL(mockHubMessageSender, SendBuzz()).Times(1);
+
+    handler.OnMessageReceived("{\"type\":\"question_open\"}");
+    buzzerBehavior.OnBuzzPressed();
 }
 
 int main(int argc, char **argv)
