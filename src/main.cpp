4f93961ff29e81957eda8ee64eb171edf72c3f4b
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-06-20 22:48:22 +0200

    feat(main): put the buzzer into service

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,66 @@
 #include <Arduino.h>
+#include <WebSocketsClient.h>
+#include <WiFi.h>
+#include <vector>
+#include "ArduinoGpioPinReader.h"
+#include "ArduinoLocalFeedback.h"
+#include "ReconnectionPolicy.h"
+#include "WebSocketPayloadSocketWriter.h"
+#include "WebSocketMessageSender.h"
+#include "PhysicalButtonReader.h"
+#include "BuzzerBehavior.h"
+#include "TokenSessionMessageSender.h"
+#include "BuzzerButtonPressTranslator.h"
+#include "HubMessageDispatcher.h"
+#include "ButtonPressDetector.h"
+#include "ConnectionEventHandler.h"
+#include "BuzzerEventTranslator.h"
+#include "WebSocketEventListener.h"
 
-void setup() {}
-void loop() {}
+ArduinoGpioPinReader pinAReader(1);
+ArduinoGpioPinReader pinBReader(2);
+ArduinoGpioPinReader pinCReader(3);
+ArduinoGpioPinReader pinDReader(4);
+ArduinoGpioPinReader pinBuzzReader(5);
+std::vector<ButtonPin> buttonPins{
+    {pinAReader, PhysicalButton::A},
+    {pinBReader, PhysicalButton::B},
+    {pinCReader, PhysicalButton::C},
+    {pinDReader, PhysicalButton::D},
+    {pinBuzzReader, PhysicalButton::Buzz},
+};
+ArduinoLocalFeedback localFeedback{{6, 7, 8, 9}};
+ReconnectionPolicy reconnectionPolicy;
+WebSocketsClient client;
+WebSocketPayloadSocketWriter payloadSocketWriter{client};
+WebSocketMessageSender messageSender{MessageSerializer{}, client};
+PhysicalButtonReader physicalButtonReader{buttonPins};
+BuzzerBehavior buzzerBehavior{messageSender, localFeedback};
+TokenSessionMessageSender tokenSessionMessageSender{payloadSocketWriter, MessageSerializer{}};
+BuzzerButtonPressTranslator buzzerButtonPressTranslator{buzzerBehavior};
+HubMessageDispatcher hubMessageDispatcher{buzzerBehavior};
+ButtonPressDetector buttonPressDetector{physicalButtonReader, buzzerButtonPressTranslator};
+ConnectionEventHandler connectionEventHandler{reconnectionPolicy, hubMessageDispatcher, tokenSessionMessageSender, tokenSessionMessageSender};
+BuzzerEventTranslator buzzerEventTranslator{connectionEventHandler};
+WebSocketEventListener webSocketEventListener{client, buzzerEventTranslator};
+
+void setup()
+{
+    pinAReader.Begin();
+    pinBReader.Begin();
+    pinCReader.Begin();
+    pinDReader.Begin();
+    pinBuzzReader.Begin();
+    localFeedback.Begin();
+    webSocketEventListener.Begin();
+    WiFi.begin("VOTRE_SSID", "VOTRE_MOT_DE_PASSE");
+    while (WiFi.status() != WL_CONNECTED)
+        delay(500);
+    client.begin("192.168.1.83", 8282, "/ws");
+}
+
+void loop()
+{
+    webSocketEventListener.Loop();
+    buttonPressDetector.Poll();
+}
