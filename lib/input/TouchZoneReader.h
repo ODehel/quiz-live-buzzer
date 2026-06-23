763e2105b086c69dc6b11ad350a599c24ef42fca
Olivier Dantas Lima <olivier.dantaslima@gmail.com>
2026-06-23 20:25:46 +0200

    chore(input): deduce the pressed button from a touch point

---
+++ lib/input/TouchZoneReader.h
@@ -0,0 +1,44 @@
+#ifndef TOUCH_ZONE_READER_H
+#define TOUCH_ZONE_READER_H
+
+#include <vector>
+#include "ButtonInput.h"
+
+struct TouchPoint
+{
+    int x;
+    int y;
+};
+
+struct TouchZone
+{
+    int left;
+    int top;
+    int right;
+    int bottom;
+    ButtonInput button;
+};
+
+class TouchZoneReader
+{
+private:
+    std::vector<TouchZone> &zones;
+
+public:
+    TouchZoneReader(std::vector<TouchZone> &zones) : zones(zones)
+    {
+    }
+
+    ButtonInput DeduceButton(TouchPoint point)
+    {
+        for (TouchZone &zone : zones)
+        {
+            if (point.x >= zone.left && point.x <= zone.right &&
+                point.y >= zone.top && point.y <= zone.bottom)
+                return zone.button;
+        }
+        return ButtonInput::Unknown;
+    }
+};
+
+#endif
---
+++ test/test_touch_zone_reader/test_touch_zone_reader.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include "TouchZoneReader.h"
+#include "ButtonInput.h"
+
+TEST(TouchZoneReaderTest, PointInsideZoneDeducesItsButton)
+{
+    std::vector<TouchZone> zones{{0, 0, 100, 100, ButtonInput::A}};
+    TouchZoneReader reader{zones};
+
+    EXPECT_EQ(reader.DeduceButton({50, 50}), ButtonInput::A);
+}
+
+TEST(TouchZoneReaderTest, PointOutsideZoneDeducesUnknown)
+{
+    std::vector<TouchZone> zones{{0, 0, 100, 100, ButtonInput::A}};
+    TouchZoneReader reader{zones};
+
+    EXPECT_EQ(reader.DeduceButton({200, 200}), ButtonInput::Unknown);
+}
+
+TEST(TouchZoneReaderTest, PointRightOfZoneDeducesUnknown)
+{
+    std::vector<TouchZone> zones{{0, 0, 100, 100, ButtonInput::A}};
+    TouchZoneReader reader{zones};
+
+    EXPECT_EQ(reader.DeduceButton({150, 50}), ButtonInput::Unknown);
+}
+
+TEST(TouchZoneReaderTest, PointLeftOfZoneDeducesUnknown)
+{
+    std::vector<TouchZone> zones{{0, 0, 100, 100, ButtonInput::A}};
+    TouchZoneReader reader{zones};
+
+    EXPECT_EQ(reader.DeduceButton({-2, 50}), ButtonInput::Unknown);
+}
+
+TEST(TouchZoneReaderTest, PointBelowOfZoneDeducesUnknown)
+{
+    std::vector<TouchZone> zones{{0, 0, 100, 100, ButtonInput::A}};
+    TouchZoneReader reader{zones};
+
+    EXPECT_EQ(reader.DeduceButton({50, 250}), ButtonInput::Unknown);
+}
+
+TEST(TouchZoneReaderTest, PointAboveOfZoneDeducesUnknown)
+{
+    std::vector<TouchZone> zones{{0, 0, 100, 100, ButtonInput::A}};
+    TouchZoneReader reader{zones};
+
+    EXPECT_EQ(reader.DeduceButton({50, -2}), ButtonInput::Unknown);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
